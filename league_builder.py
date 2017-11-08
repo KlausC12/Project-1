7eb7938299deac5a157063c9bda2c9dc997828aa
Klaus Cleaver <klausc12@gmail.com>
2017-11-08 15:30:29 -0500

    Letter Writing Finished
    Completed letter info/writing functions. Added more comments. Added datetime.

--- league_builder.py
+++ league_builder.py
@@ -1,5 +1,6 @@
 import random
 import csv
+from datetime import date, timedelta
 
 
 def exp_sorter(players):    # sorts players into two lists - experience vs no experience
@@ -32,25 +33,41 @@ def team_list(*teams):    # accepts final team lists and writes them to txt file
             count += 1
 
 
-def letter_info(*teams):     # include team name, deal with name append issue
+def letter_info(*teams):    # gathers player data, team name, creates txt file for use in intro letter
     name_list = []
     for team in teams:
+        for player in team:    # updates team names to player data
+            if player in dragons:
+                player.update({'Team': 'Dragons'})
+            elif player in raptors:
+                player.update({'Team': 'Raptors'})
+            elif player in sharks:
+                player.update({'Team': 'Sharks'})
+
         for player in team:
-            name_list.append([player['Name'].lower(), player])
-    for player in name_list:
+            for item in player.items():    # includes name for txt file and player data
+                if item[0] == 'Name':
+                    name_list.append([item[1].lower(), player])
+
+    for player in name_list:    # creates txt file based on player name
         player[0] = player[0].split()
         player[0] = '_'.join(player[0]) + '.txt'
         letter_list.append(player)
 
 
-def intro_letter(names):
+def letter_writer(names):   # creates a new txt file for each player on the roster
     for player in names:
         with open(player[0], 'w') as file:
             file.write('Dear Mr. and/or Mrs. ' + player[1]['Guardian Name(s)'] + ',\n\n')
-            file.write('Your child, ' + player[1]['Name'] + ' has been placed on the '
+            file.write('Your child, ' + player[1]['Name'] + ', will be playing for the ' + player[1]['Team']
+                       + ' soccer team in the upcoming season.\n')
+            file.write('The first team practice will be one week from now on '
+                       + (current_date + week).strftime("%A %d, %B %Y") + ' at 4:00 pm.\n')
 
+            first_name = player[1]['Name'].split()
+            first_name = first_name[0]
 
-                       )
+            file.write('Good luck to ' + first_name + ' this year!\n\n--Youth Soccer League--')
 
 
 if __name__ == "__main__":
@@ -60,24 +77,20 @@ if __name__ == "__main__":
         player_list = list(filereader)
 
     team_names = ['Dragons', 'Raptors', 'Sharks']
-
     has_exp = []
     no_exp = []
 
-    exp_sorter(player_list)
+    exp_sorter(player_list)    # sort players
 
-    dragons = team_builder(has_exp, no_exp)
+    dragons = team_builder(has_exp, no_exp)    # build teams
     raptors = team_builder(has_exp, no_exp)
     sharks = team_builder(has_exp, no_exp)
 
-    team_list(dragons, raptors, sharks)
+    team_list(dragons, raptors, sharks)    # create team file
 
     letter_list = []
-    letter_info(dragons, raptors, sharks)
+    current_date = date.today()
+    week = timedelta(days=7)
 
-    intro_letter(letter_list)
-
-
-
-
-
+    letter_info(dragons, raptors, sharks)    # gather info
+    letter_writer(letter_list)    # write letters
